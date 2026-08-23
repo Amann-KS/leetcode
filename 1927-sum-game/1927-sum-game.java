7a7e3db7904c7e8c6e54da96d5e0f69fd5867fc4
Aman Kumar Singh <2k23.cscys2311513@gmail.com>
2026-08-23 20:37:14 +0530

    Time: 8 ms (53.28%), Space: 47.1 MB (41.61%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int sumDiff = 0;
+        int qDiff = 0;
+
+        for (int i = 0; i < n; i++) {
+            char c = num.charAt(i);
+            int sign = (i < n / 2) ? 1 : -1;
+
+            if (c == '?') {
+                qDiff += sign;
+            } else {
+                sumDiff += sign * (c - '0');
+            }
+        }
+
+        if ((qDiff & 1) != 0) {
+            return true;
+        }
+
+        
+        return sumDiff * 2 + qDiff * 9 != 0;
+    }
+}
